ccb6ad72abf844414933c0e3c99aec1215a572c3
shivamraj973 <shivamraj1727@gmail.com>
2025-04-11 14:09:51 +0530

    Add files via upload

---
+++ DAAproject/Shivam Raj/index.js
@@ -0,0 +1,14 @@
+function solveKnapsack() {
+    window.location.href = 'Knapsack.html';
+}
+
+function solveCoinChange() {
+    window.location.href = 'coinchange.html';
+}
+
+function multiplyMatrices() {
+    window.location.href = 'Matrix.html'
+}
+document.getElementById("knapsackBtn").addEventListener("click", solveKnapsack);
+document.getElementById("coinChangeBtn").addEventListener("click", solveCoinChange);
+
